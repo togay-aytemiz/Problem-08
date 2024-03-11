e1f0864e764c792b075c1b7b9e18ff9764cf36ac
togay-aytemiz <togayaytemiz@gmail.com>
2024-03-11 17:32:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+// Statik değerleri alın ve prop'ları kullanarak dinamik hale getirin
+// Örneğin: label id, label text, input placeholder, input description
+function FormInput(props) {
+  const { labelID, labelText, placeholder, desc } = props;
+
+  return (
+    <div className="max-w-sm p-8">
+      <label
+        htmlFor="email"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        {labelText}
+      </label>
+      <div className="mt-2">
+        <input
+          id={labelID}
+          name={labelID}
+          type={labelID}
+          placeholder={placeholder}
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
+        />
+      </div>
+      <p className="mt-2 text-sm text-gray-500">{desc}</p>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <form>
+      <FormInput
+        labelID="email"
+        labelText="Email"
+        placeholder="you@example.com"
+        desc="Bunu sadece spam için kullanacağız."
+      />
+      <FormInput
+        labelID="password"
+        labelText="Password"
+        placeholder="Şifreniz"
+        desc="Kolay bir şey seçin, örneğin 1234..."
+      />
+    </form>
+  );
+}
